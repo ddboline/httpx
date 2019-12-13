d331b10064bfb10ea94e45dc33b3c8a3acbb3e0f
Daniel Boline <ddboline@gmail.com>
2019-12-13 12:15:11 -0500

    ...

--- setup.py
+++ setup.py
@@ -53,12 +53,12 @@ setup(
     install_requires=[
         "certifi",
         "hstspreload",
-        "chardet==3.*",
-        "h11==0.8.*",
-        "h2==3.*",
-        "idna==2.*",
-        "rfc3986>=1.3,<2",
-        "sniffio==1.*",
+        "chardet=>3.0",
+        "h11>=0.8",
+        "h2>=3.0",
+        "idna>=2.0",
+        "rfc3986>=1.3",
+        "sniffio>=1.0",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
